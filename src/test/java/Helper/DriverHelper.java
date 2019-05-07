04a3cdde093a2d3dd74417121eb090e01b993263
Cibele Silva <cibeleosilva@gmail.com>
2019-05-07 13:18:21 -0300

    pluggins

---
+++ src/test/java/Helper/DriverHelper.java
@@ -0,0 +1,11 @@
+package Helper;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverHelper {
+
+    private final String CHROMEDRIVER_PATH = "src/test/resource/chromedriver.exe";
+    private final String GECKODRIVER_PATH = "src/test/resource/geckodriver.exe";
+
+
+}
---
+++ src/test/java/pageObjects/CadastroPO.java
@@ -0,0 +1,10 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class CadastroPO extends MasterPO {
+    public CadastroPO(WebDriver driver) {
+        super(driver);
+    }
+}
+
